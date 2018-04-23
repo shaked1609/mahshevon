e639c687f1ff474b7976ebbfa9eb4d6bacdb0ff3
user <aesaes123121@gmail>
2018-04-23 14:01:45 +0300

    commit1

---
+++ app/src/main/java/com/example/user/shaked/Credi.java
@@ -0,0 +1,13 @@
+package com.example.user.shaked;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Credi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credi);
+    }
+}
